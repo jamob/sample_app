1b77134f182042332bc3ecf3fca44c05c82c9f89
Jamob <tspurdle18@gmail.com>
2012-05-31 12:24:20 -0400

    Finish user, edit, update, index and delete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_name(params[:session][:name])
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = "Invalid name/password combination"
       render 'new'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name      "Foo Bar"
-    email     "foo@bar.org"
-    password  "foobaz"
-    password_confirmation "foobaz"
+    sequence(:name) {|n| "Person #{n}"}
+    sequence(:email){|n| "person_#{n}@example.com"}
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
